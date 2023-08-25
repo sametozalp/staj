56798fe23904cca393bec889fbab56d69588fe98
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-08-25 16:36:15 +0300

    k

---
+++ struct-union/struct-union/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace struct_union
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            @struct s = new @struct();
+
+            s.vol = 42;
+        }
+
+        struct @struct
+        {
+            public uint vol;
+            public struct2 adr;
+
+            public struct struct2
+            {
+                public uint vol2;
+                public uint vol3;
+                public uint vol4;
+                public uint vol5;
+                public uint vol6;
+                public uint vol7;
+            }
+        }
+    }
+}
