12582bbbfd019db2420b9ae324874790d93e7f88
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-09-11 15:38:15 +0300

    t

---
+++ Debug/Debug/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Debug
+{
+    using System;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Hesap Makinesi");
+
+            Console.Write("Bolunen sayıyı girin: ");
+            int sayi1 = 6;
+
+            Console.Write("Bolen sayıyı girin: ");
+            int sayi2 = 3;
+
+            int bolum = sayi1 / sayi2;
+            Console.WriteLine("Bolum: " + bolum);
+            */
+
+            for (int i = 0; i < 10; i++) { 
+                var deneme = i*10;
+            }
+        }
+    }
+
+}
