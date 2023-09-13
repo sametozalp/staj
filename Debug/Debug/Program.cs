0a0e911559add1838559cc4a32459e9486fc2f96
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-09-13 11:07:14 +0300

    y

--- Debug/Debug/Program.cs
+++ Debug/Debug/Program.cs
@@ -3,27 +3,38 @@
 namespace Debug
 {
     using System;
+    using System.Threading;
 
     public class Program
     {
         static void Main(string[] args)
         {
-            /*
-            Console.WriteLine("Hesap Makinesi");
+            
+            int[] array = new int[6];
+            for (int i = 0; i < 5; i++) {
+                array[i] = i*10;
+            }
+
+            //**************************************
+
+            Thread thread = new Thread(fonk);
+            thread.Start();
+
+            
+        }
+
+        public static void fonk()
+        {
+            Console.WriteLine("Bolum Makinesi");
 
             Console.Write("Bolunen sayıyı girin: ");
             int sayi1 = 6;
-            
+
             Console.Write("Bolen sayıyı girin: ");
             int sayi2 = 3;
 
             int bolum = sayi1 / sayi2;
             Console.WriteLine("Bolum: " + bolum);
-            */
-            int[] array = new int[80];
-            for (int i = 0; i < 10; i++) {
-                array[i] = i*10;
-            }
         }
     }
 }
