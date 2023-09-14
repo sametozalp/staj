68ab5350a5e3b9fe06dbf74f13efa0383feb2ef2
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-09-14 10:08:23 +0300

    b

--- UART1/UART1/Program.cs
+++ UART1/UART1/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace UART1
+﻿using System;
+using System.IO.Ports;
+
+namespace UART1 
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            SerialPort serialPort = new SerialPort();
+            Console.WriteLine(SerialPort.GetPortNames());
+            SerialPort serialPort = new SerialPort("COM5",9600);
+
+            try
+            {
+                serialPort.Open();
+
+                while (true)
+                {
+                    string data = serialPort.ReadLine();
+                    Console.WriteLine("Gelen veri " + data);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message);
+            }
+            finally
+            {
+                serialPort.Close();
+            }
         }
     }
 }
